60bbe16244024f8c6d43a6493f40a89b01b69754
huayuwu_windows <3158677545@qq.com>
2023-04-02 00:12:34 +0800

    reuqest body

--- tmp.py
+++ tmp.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Query, Path, Body
+from pydantic import BaseModel, Required
+from typing import Union
 import uvicorn
 
 app = FastAPI()
@@ -7,16 +8,50 @@ app = FastAPI()
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+class Item(BaseModel):
+    name: str
+    desc: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=100),
+    q: str = Query(
+        default=Required, description="testsette", title="Query string", max_length=30
+    ),
+):
+    results = {"items": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.put("/item/{item_id}")
+@app.get("/item/{item_id}")
+async def update_items(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: Union[str, None] = None,
+    item: Union[Item, None] = None
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results
+
+
+@app.post("/post")
+async def create_iem(item: Item = Body(embed=True)):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
 
 if __name__ == "__main__":
-    # uvicorn.run(app=app, host="127.0.0.1", port=8082)
-    ...
+    uvicorn.run(app="tmp:app", host="127.0.0.1", port=8888, reload=True)
